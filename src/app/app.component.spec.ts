72488e208c36026f93f62f1495d1f0ab611d2708
ichsanashiddiqi <iashiddiqi13@gmail.com>
2020-09-21 20:59:02 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'register'`, () => {
+  it(`should have as title 'register-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('register');
+    expect(app.title).toEqual('register-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('register app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('register-frontend app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
